3cdbfe8cba46809df90cd3dc7fcff493d71b4021
Priyanka8433 <priyuchaudhary64@gmail.com>
2025-08-24 20:56:39 +0530

    Update Divsibleby5and11.py

--- Week1/Divsibleby5and11.py
+++ Week1/Divsibleby5and11.py
@@ -1,4 +1,5 @@
-n=int(input())
-m=int(input())
-for i in range(0,n):
-    for j in range()
+a=int(input("enter the number:"))
+if(a % 5==0 and a % 11==0):
+    print("Divisble by 5 and 11")
+else:
+    print("Not Divisible by 5 and 11")
